918c789f822c3f0da7402eb3e8321bda9769a593
Lauren Gilman <lauren.gilman@yahoo.co.uk>
2022-02-20 20:24:49 +0000

    added more rows

--- populatedatabase.sql
+++ populatedatabase.sql
@@ -11,5 +11,11 @@ VALUES ("Bot Platform", 1540, 360);
 
 INSERT INTO member (member_name, member_email, parent_organisation_id)
 VALUES ("Bob Smith", "bob@trilateral.com", 1);
-
-
+INSERT INTO member (member_name, member_email, parent_organisation_id)
+VALUES ("Angela Bobs", "Angela@trilateral.com", 1);
+INSERT INTO member (member_name, member_email, parent_organisation_id)
+VALUES ("Ryan Reynolds", "ryan@TB.com", 2);
+INSERT INTO member (member_name, member_email, parent_organisation_id, line_manager_id)
+VALUES ("Andres Boyd", "Andes@TB.com", 2, 3);
+INSERT INTO member (member_name, member_email, parent_organisation_id)
+VALUES ("Chris Hampson", "chris@TB.com", 2);
