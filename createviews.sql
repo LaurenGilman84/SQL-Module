45015bf7991b6334fe000ebec5df9aa2c5e9a761
Lauren Gilman <lauren.gilman@yahoo.co.uk>
2022-02-24 16:54:14 +0000

    Altered to actually work!

--- createviews.sql
+++ createviews.sql
@@ -17,19 +17,11 @@ SELECT organisation.organisation_id, organisation.organisation_name, member.memb
 FROM organisation, member
 WHERE organisation.organisation_id = parent_organisation_id;
 
-/*Creating a view for the Trilateral HR Manager to see which staff members are enrolled on which courses and which modules*/ 
-
-
-CREATE VIEW TrilateralStaff (organisation_id, member_id, member_name, course_id, course_name, module_id, module_name) AS
-SELECT organisation.organisation_id, member.member_id, member.member_name, course.course_id, course.course_name, module.module_id, module.module_name
-FROM organisation, member, content, course, module
-WHERE organisation.organisation_id = 1 AND organisation.organisation_id = member.parent_organisation_id AND content.course_id = course.course_id AND content.module_id = module.module_id;
-
 
 /*Creating a view for the Trilateral HR Manager to see staff scores for all modules taken*/ 
 
 
-CREATE VIEW TrilateralScores (organisation_id, member_id, member_name, course_id, course_name, module_id, module_name, student_module_score) AS
-SELECT organisation.organisation_id, member.member_id, member.member_name, course.course_id, course.course_name, module.module_id, module.module_name, results.student_module_score
-FROM organisation, member, content, course, module, results
-WHERE organisation.organisation_id = 1 AND organisation.organisation_id = member.parent_organisation_id AND content.course_id = course.course_id AND content.module_id = module.module_id AND results.module_id = module.module_id AND results.student_module_score IS NOT NULL;
+CREATE VIEW TrilateralScores (member_name, module_id, module_name, student_module_score) AS
+SELECT  member.member_name, results.module_id, module.module_name, results.student_module_score
+FROM organisation, results, module, member
+WHERE organisation.organisation_id = 1 AND member.member_id = results.student_id AND results.student_module_score IS NOT NULL AND module.module_id = results.module_id;
--- queryexamples.sql
+++ queryexamples.sql
@@ -16,3 +16,39 @@ SELECT * FROM results WHERE student_passed = 1;
 SELECT * FROM content WHERE course_id = 3;
 
 
+/*Viewing all the rows contained in the TrilateralScores view*/
+
+SELECT * FROM TrilateralScores;
+
+
+/*Querying a view to find everyone in Trilateral who has scored under the pass mark of 20 in a module*/
+
+SELECT member_name, module_name, student_module_score
+FROM TrilateralScores
+WHERE student_module_score < 20;
+
+
+/*Dropping a view*/
+
+DROP VIEW TrilateralList;
+
+/*Creating a joint table of all the course modules and their names*/
+
+SELECT content.course_id, course.course_name, module.module_id, module.module_name
+FROM content, course, module
+WHERE content.course_id = course.course_id AND content.module_id = module.module_id;
+
+/*creating a view of scores over 40 ordered by highest first*/
+
+SELECT organisation.organisation_name, module.module_name, member.member_name, results.student_module_score
+FROM organisation, results, member, module
+WHERE results.student_module_score >40 AND results.student_id = member.member_id AND organisation.organisation_id = member.parent_organisation_id AND results.module_id = module.module_id
+ORDER BY results.student_module_score DESC;
+
+/* creating a list of a managers direct reports and their module scores*/
+
+SELECT  member.member_name, results.module_id, module.module_name, results.student_module_score
+FROM relationships, results, module, member
+WHERE relationships.line_manager_id = 6 AND relationships.member_rel_id = results.student_id AND results.student_module_score IS NOT NULL AND module.module_id = results.module_id AND member.member_id = results.student_id;
+
+
